243bcdd1111eb54ef42bff4a5400e100bd1df71e
talik87 <talik87@gmail.com>
2019-11-18 22:19:11 +0200

    complete app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import './App.css';
+import { CSSTransition } from "react-transition-group";
+
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: [],
+      photos: [],
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(res => res.json())
+      .then(albums => {
+        this.setState({ albums })
+        console.log(albums)
+      })
+  }
+  getPhotos(event) {
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${event.target.value}`)
+      .then(res => res.json())
+      .then(photos => {
+        this.setState({ photos });
+        console.log(photos)
+      });
+  }
+
 
   render() {
     return (
-        <div className="App">
-          <h1>Select an album:</h1>
-
+      <div className="App">
+        <h1 className="app-title">Select an album:</h1>
+        <select onChange={this.getPhotos.bind(this)} >
+          {this.state.albums.map(item => {
+            return <option value={item.id} key={item.id}>{item.title}</option>
+          })}
+        </select>
+        <div className="app-photos">
+          {this.state.photos.map(photo => {
+            return <CSSTransition
+              key={photo.id}
+              in={true}
+              appear={true}
+              timeout={1000}
+              classNames="fade">
+              <img src={photo.thumbnailUrl} alt={photo.title} key={photo.id} />
+            </CSSTransition>
+          })}
         </div>
+
+      </div>
     );
   }
 
